ffb974b011ffb0920eccb1ff68188e425d5dafbc
CodeAldi <codealdi@gmail.com>
2022-03-30 03:21:02 +0700

    semua beres kecuali delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/edit'
+  resources :items
+  # get 'items/index'
+  # get 'items/show'
+  # get 'items/new'
+  # get 'items/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_111105) do
+ActiveRecord::Schema.define(version: 2022_03_29_170539) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
